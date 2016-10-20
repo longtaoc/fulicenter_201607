5e7937465aa7c5fe0939fd6caec630140c1cdc67
Long Tao <longtaojay@163.com>
2016-10-20 23:22:59 +0800

    在MainActivity实现互斥

--- app/src/main/java/cn/ucai/fulicenter/activity/MainActivity.java
+++ app/src/main/java/cn/ucai/fulicenter/activity/MainActivity.java
@@ -3,19 +3,75 @@ package cn.ucai.fulicenter.activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.RadioButton;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import cn.ucai.fulicenter.R;
 import cn.ucai.fulicenter.utils.L;
 
 public class MainActivity extends AppCompatActivity {
 
+    @BindView(R.id.layout_new_good)
+    RadioButton layoutNewGood;
+    @BindView(R.id.layout_boutique)
+    RadioButton layoutBoutique;
+    @BindView(R.id.layout_category)
+    RadioButton layoutCategory;
+    @BindView(R.id.layout_cart)
+    RadioButton layoutCart;
+    @BindView(R.id.layout_personal_center)
+    RadioButton layoutPersonalCenter;
+
+    int index;
+    RadioButton [] rbs;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
         L.i("MainActivity onCreate");
+        initView();
     }
-    public void onCheckedChange(View v){
 
+    private void initView() {
+        rbs=new RadioButton[5];
+        rbs[0] =layoutNewGood;
+        rbs[1] =layoutBoutique;
+        rbs[2] =layoutCategory;
+        rbs[3] =layoutCart;
+        rbs[4] =layoutPersonalCenter;
+    }
+
+    public void onCheckedChange(View v) {
+        switch (v.getId()){
+            case R.id.layout_new_good:
+                index=0;
+                break;
+            case R.id.layout_boutique:
+                index=1;
+                break;
+            case R.id.layout_category:
+                index=2;
+                break;
+            case R.id.layout_cart:
+                index=3;
+                break;
+            case R.id.layout_personal_center:
+                index=4;
+                break;
+        }
+        setRadioButtonStatus();
+    }
+
+    private void setRadioButtonStatus() {
+        for (int i=0;i<rbs.length;i++) {
+            if (i == index) {
+                rbs[i].setChecked(true);
+            }else {
+                rbs[i].setChecked(false);
+            }
+        }
     }
 }
