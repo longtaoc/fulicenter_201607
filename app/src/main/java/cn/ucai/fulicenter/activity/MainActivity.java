cc9d28cc5f93419caa53c50c02e62961852fd8ca
Long Tao <longtaojay@163.com>
2016-10-20 20:42:01 +0800

    底部菜单栏选中点击

--- app/src/main/java/cn/ucai/fulicenter/activity/MainActivity.java
+++ app/src/main/java/cn/ucai/fulicenter/activity/MainActivity.java
@@ -1,7 +1,8 @@
 package cn.ucai.fulicenter.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import cn.ucai.fulicenter.R;
 import cn.ucai.fulicenter.utils.L;
@@ -13,5 +14,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         L.i("MainActivity onCreate");
+    }
+    public void onCheckedChange(View v){
+
     }
 }
