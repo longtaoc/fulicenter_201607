45d2f1a14a8582eed6b31b5cfdfe6d7b7cc71e27
Long Tao <longtaojay@163.com>
2016-10-20 11:52:29 +0800

    修改闪屏跳转主页面的逻辑，跳转后直接结束跳转。

--- app/src/main/java/cn/ucai/fulicenter/activity/SplashActivity.java
+++ app/src/main/java/cn/ucai/fulicenter/activity/SplashActivity.java
@@ -12,7 +12,7 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash2);
+        setContentView(R.layout.activity_splash);
     }
 
     @Override
@@ -32,6 +32,7 @@ public class SplashActivity extends AppCompatActivity {
                     }
                 }
                 startActivity(new Intent(SplashActivity.this,MainActivity.class));
+                finish();
             }
         }).start();
 
