134467f7e221ecd1d9a9c867586a5445c29e5b66
Long Tao <longtaojay@163.com>
2016-10-25 23:20:18 +0800

    商品详情的数据填充

--- app/src/main/java/cn/ucai/fulicenter/activity/GoodsDetailActivity.java
+++ app/src/main/java/cn/ucai/fulicenter/activity/GoodsDetailActivity.java
@@ -8,13 +8,16 @@ import android.widget.TextView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import cn.ucai.fulicenter.I;
 import cn.ucai.fulicenter.R;
+import cn.ucai.fulicenter.bean.AlbumsBean;
 import cn.ucai.fulicenter.bean.GoodsDetailsBean;
 import cn.ucai.fulicenter.net.NetDao;
 import cn.ucai.fulicenter.net.OkHttpUtils;
 import cn.ucai.fulicenter.utils.CommonUtils;
 import cn.ucai.fulicenter.utils.L;
+import cn.ucai.fulicenter.utils.MFGT;
 import cn.ucai.fulicenter.view.FlowIndicator;
 import cn.ucai.fulicenter.view.SlideAutoLoopView;
 
@@ -77,8 +80,32 @@ public class GoodsDetailActivity extends AppCompatActivity {
                 mTvGoodName.setText(details.getGoodsName());
                 mTvGoodPriceCurrent.setText(details.getCurrencyPrice());
                 mTvGoodPriceShop.setText(details.getShopPrice());
+                mSalv.startPlayLoop(mIndicator,getAlbumImgUrl(details),getAlbumImgCount(details));
+                mWvGoodBrief.loadDataWithBaseURL(null,details.getGoodsBrief(),I.TEXT_HTML,I.UTF_8,null);
 
             }
+
+            private int getAlbumImgCount(GoodsDetailsBean details) {
+                if (details.getProperties()!=null && details.getPromotePrice().length()>0) {
+                   return details.getProperties()[0].getAlbums().length;
+                }
+                return 0;
+            }
+
+            private String[] getAlbumImgUrl(GoodsDetailsBean details) {
+                String [] urls= new String[]{};
+                if (details.getProperties()!=null && details.getPromotePrice().length()>0){
+                    AlbumsBean[] albums=details.getProperties()[0].getAlbums();
+                    urls=new String[albums.length];
+                    for (int i=0;i<albums.length;i++){
+                        urls[i]=albums[i].getImgUrl();
+                    }
+                    return urls;
+
+                }
+                return new String[0];
+            }
+
             @Override
             public void onError(String error) {
                 finish();
@@ -91,5 +118,15 @@ public class GoodsDetailActivity extends AppCompatActivity {
     }
     private void initView() {
 
+    }
+    @OnClick(R.id.backClickArea)
+    public void onBackClick(){
+        MFGT.finish(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        MFGT.finish(this);
     }
 }
